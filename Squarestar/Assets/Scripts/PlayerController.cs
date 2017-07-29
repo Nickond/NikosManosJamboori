e9104045bcfbebaa40164ec8e8e4596ebc40218d
Emmanuel Miras <prezolov@live.com>
2017-07-29 17:07:57 +0100

    Player shooting

--- Squarestar/Assets/Scripts/PlayerController.cs
+++ Squarestar/Assets/Scripts/PlayerController.cs
@@ -12,9 +12,11 @@ public class PlayerController : MonoBehaviour {
     public float speedSmoothTime = 0.1f;
     float speedSmoothVelocity;
     float currentSpeed;
-
+    public GameObject playerProjectile;
+    public Transform projectileStartPoint;
+    public float projectileSpeed;
     Animator animator;
-
+    private float totalTime = 0;
     void Start()
     {
         animator = GetComponent<Animator>();
@@ -25,7 +27,7 @@ public class PlayerController : MonoBehaviour {
 
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector2 inputDir = input.normalized;
-
+        
         if (inputDir != Vector2.zero)
         {
             float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
@@ -40,8 +42,23 @@ public class PlayerController : MonoBehaviour {
 
         float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
         animator.SetFloat("speed", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
-        animator.SetBool("shoot",Input.GetMouseButton(0));
-        
+
+        bool shooting = Input.GetMouseButton(0);
+        animator.SetBool("shoot", shooting);
+        if (shooting)
+        {
+            // Cooldown
+            totalTime += Time.deltaTime;
+            //Debug.Log(totalTime);
+            // Magic number
+            if (totalTime >= 0.35714285714285714285714285714286)
+            {
+                // Spawn bullets
+                GameObject newProjectile = Instantiate(playerProjectile, projectileStartPoint.position, transform.rotation) as GameObject;
+                // Reset time
+                totalTime = 0;
+            }
+        }
 
     }
 }
