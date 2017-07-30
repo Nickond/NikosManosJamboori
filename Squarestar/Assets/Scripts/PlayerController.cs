c4c5778a08234059e207cef4c2799b6a17498b07
Emmanuel Miras <prezolov@live.com>
2017-07-30 17:31:19 +0100

    Targetting and shooting sound

--- Squarestar/Assets/Scripts/PlayerController.cs
+++ Squarestar/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
+[RequireComponent(typeof(AudioSource))]
 public class PlayerController : MonoBehaviour {
 
     public float walkSpeed = 2;
@@ -17,9 +18,23 @@ public class PlayerController : MonoBehaviour {
     public float projectileSpeed;
     Animator animator;
     private float totalTime = 0;
+    private Quaternion aimingRotation;
+    private Image targetImage;
+    private Camera mainCamera;
+    public AudioClip shot;
+    AudioSource audioSource;
     void Start()
     {
         animator = GetComponent<Animator>();
+
+        GameObject go = GameObject.Find("Canvas");
+        if (!go)
+            return;
+
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        audioSource = GetComponent<AudioSource>();
+
+        targetImage = go.GetComponentInChildren<Image>();
     }
 
     void Update()
@@ -28,7 +43,25 @@ public class PlayerController : MonoBehaviour {
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector2 inputDir = input.normalized;
         
-        if (inputDir != Vector2.zero)
+        // Get the current rotation
+        if (Input.GetMouseButtonDown(1))
+        {
+            aimingRotation = transform.rotation;
+        }
+
+        // Aiming, lock rotation
+        bool aiming = Input.GetMouseButton(1);
+        if (aiming)
+        {
+            transform.rotation = aimingRotation;
+        }
+        // Target
+        targetImage.enabled = aiming;
+        // Position target based on player rotation and position
+        Vector3 screenPos = mainCamera.WorldToScreenPoint(transform.position + transform.forward * 4); //targetImage.rectTransform.
+        targetImage.rectTransform.position = screenPos;
+        Debug.Log(targetImage.rectTransform.offsetMax.y);
+        if (inputDir != Vector2.zero && !aiming)
         {
             float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
             transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
@@ -38,7 +71,8 @@ public class PlayerController : MonoBehaviour {
         float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
         currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
 
-        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        //transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")) * currentSpeed * Time.deltaTime, Space.World);
 
         float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
         animator.SetFloat("speed", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
@@ -55,10 +89,12 @@ public class PlayerController : MonoBehaviour {
             {
                 // Spawn bullets
                 GameObject newProjectile = Instantiate(playerProjectile, projectileStartPoint.position, transform.rotation) as GameObject;
+                // Play shot sound
+                AudioSource audio = GetComponent<AudioSource>();
+                audioSource.PlayOneShot(shot, 0.7F);
                 // Reset time
                 totalTime = 0;
             }
         }
-
     }
 }
